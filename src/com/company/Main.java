6357942328172836b4c9179eca3761ceae9f3c75
nazgulnurtaeva1 <nazgulnurtaeva1@gmail.com>
2022-01-26 19:56:19 -0500

    the red car is ready for the trial

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,7 +4,11 @@ public class Main {
     public static void main(String[] args) {
 
         CarBasics car = new CarBasics();
-
+car.color="red";
+car.door="2";
+car.engine="89483c";
+car.number="f2455";
+        System.out.println(car.color);
 
     }
 }
