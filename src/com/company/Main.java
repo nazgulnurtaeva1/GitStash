caf6e3717f3772d95754add732a1683a4329ad22
nazgulnurtaeva1 <nazgulnurtaeva1@gmail.com>
2022-01-26 20:57:43 -0500

    I have made some changes in Main

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,17 +4,11 @@ public class Main {
     public static void main(String[] args) {
 
         CarBasics car = new CarBasics();
-<<<<<<< Updated upstream
-car.color="red";
-car.door="2";
-car.engine="89483c";
-car.number="f2455";
-=======
         car.number="hd7346";
         car.engine="ffghfhd";
         car.door="3";
         car.color="green";
->>>>>>> Stashed changes
+
         System.out.println(car.color);
 
     }
