19d1c96b316cad32e726a4db7b373e0c6a418bba
nazgulnurtaeva1 <nazgulnurtaeva1@gmail.com>
2022-01-30 14:32:21 -0500

    here is my cars, don't ask any changesss

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,13 +4,13 @@ public class Main {
     public static void main(String[] args) {
 
         CarBasics car = new CarBasics();
-
         car.number="hd7346";
-        car.engine="ffghfhd";
-        car.door="23";
-        car.color="redddd";
+        car.engine="ffghtreefhdhgffg";
+        car.door="5";
+        car.color="greeeen";
 
         System.out.println(car.color);
+
 
     }
 }
