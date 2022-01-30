5f078640c2a05e6d406e4bf76a3e6d4d7219b40c
nazgulnurtaeva1 <nazgulnurtaeva1@gmail.com>
2022-01-30 13:43:29 -0500

    The basic structure of car without feature

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,12 +4,6 @@ public class Main {
     public static void main(String[] args) {
 
         CarBasics car = new CarBasics();
-        car.number="hd7346";
-        car.engine="ffghfhd";
-        car.door="3";
-        car.color="green";
-
-        System.out.println(car.color);
 
     }
 }
